2947a153a8dad610581a0675ebf10289f84d29b8
Alex Alanya <alexandro.alanya@gmail.com>
2019-06-06 15:23:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,5 +6,10 @@ class School
   school.add_student("Zach Morris", 9)
   school.roster
   
+  school.add_student("AC Slater", 9)
+  school.add_student("Kelly Kapowski", 10)
+  school.add_student("Screech", 11)
+  school.roster
+  
 end
 
