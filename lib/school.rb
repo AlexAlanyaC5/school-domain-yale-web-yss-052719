bd791b31b237926da6b5e8df86de441c8622bb69
Alex Alanya <alexandro.alanya@gmail.com>
2019-06-06 15:14:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def title 
+    @title
+  end
+  
+  def roster
+  end
+  
+  def add_student=(name)
+    @name = 
+  end 
+  
+  
+end
+
+school = School.new("Bayside High School")
